751114781430f7a627ed7ab28bf51c1e680a2d9a
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2023-08-11 17:28:39 +0900

    feat: incorporate cameras into main gui

--- test_gui.py
+++ test_gui.py
@@ -7,9 +7,8 @@ import numpy as np
 import pyqtgraph as pg
 from qtpy import QtCore, QtGui, QtWidgets, uic
 
-
 from SESWrapper.ses_measure import SESMeasure
-
+import framegui, webcam
 
 SES_DIR = "D:/SES_1.9.6_Win64"
 WRAPPER_PATH = "D:/SES_1.9.6_Win64/SESWrapper.dll"
@@ -21,38 +20,66 @@ uiclass, baseclass = uic.loadUiType("test_gui.ui")
 
 
 class SESDAQMain(uiclass, baseclass):
+    USER_WINDOWS: dict[str, type[QtWidgets.QWidget]] = {
+        "webcam": webcam.MainWindow,
+        "basler": framegui.MainWindow,
+    }
+
+    # When changing this, make sure to update the objectName of the buttons
+    SES_WINDOWS: tuple[str, ...] = (
+        "CalibrateVoltages",
+        "CalibrateDetector",
+        "ControlSupplies",
+        "SupplyInfo",
+        "DetectorInfo",
+    )
+
     sigFocusChanged = QtCore.Signal(int)
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("SES GUI")
+        self.setWindowTitle("1KARPES Controls")
 
-        self.measure = SESMeasure(
-            WRAPPER_PATH,
-            SES_DIR,
-            ses_instrument=SES_INSTR,
-            inst_path=INSTR_PATH,
-            verbose=True,
-            element_set="Low Pass (UPS)",
-            motorcontrol=None,
-        )
-        self.CalibrateVoltages.clicked.connect(
-            lambda: self.measure.ses.OpenGUI("CalibrateVoltages")
-        )
-        self.CalibrateDetector.clicked.connect(
-            lambda: self.measure.ses.OpenGUI("CalibrateDetector")
-        )
-        self.ControlSupplies.clicked.connect(
-            lambda: self.measure.ses.OpenGUI("ControlSupplies")
-        )
-        self.SupplyInfo.clicked.connect(lambda: self.measure.ses.OpenGUI("SupplyInfo"))
-        self.DetectorInfo.clicked.connect(
-            lambda: self.measure.ses.OpenGUI("DetectorInfo")
-        )
+        # for debugging purposes on macOS
+        if sys.platform.startswith("win"):
+            self.measure = SESMeasure(
+                WRAPPER_PATH,
+                SES_DIR,
+                ses_instrument=SES_INSTR,
+                inst_path=INSTR_PATH,
+                verbose=True,
+                element_set="Low Pass (UPS)",
+                motorcontrol=None,
+            )
+            for win_type in self.SES_WINDOWS:
+                getattr(self, win_type).clicked.connect(
+                    lambda *, v=win_type: self.measure.ses.OpenGUI(v)
+                )
+        else:
+            self.measure = None
+            for win_type in self.SES_WINDOWS:
+                getattr(self, win_type).setDisabled(True)
+
+        self._child_windows: dict[str, QtWidgets.QWidget | None] = {
+            k: None for k in self.USER_WINDOWS.keys()
+        }
+
+        self.webcam_btn.clicked.connect(lambda: self.toggle_window("webcam"))
+        self.basler_btn.clicked.connect(lambda: self.toggle_window("basler"))
+
+    def toggle_window(self, window: str):
+        if self._child_windows[window] is None:
+            self._child_windows[window] = self.USER_WINDOWS[window]()
+            self._child_windows[window].show()
+        else:
+            self._child_windows[window].close()
+            print(self._child_windows[window])
+            self._child_windows[window] = None
 
     def closeEvent(self, *args, **kwargs):
-        self.measure.Finalize()
+        if self.measure is not None:
+            self.measure.Finalize()
         super().closeEvent(*args, **kwargs)
 
 
