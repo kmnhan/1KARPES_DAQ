8d98d6263fcbad04d17a5efb1ca9a55fd2a40837
Kimoon Han <khan@kaist.ac.kr>
2023-11-06 16:46:34 +0900

    initial server impl

--- src/motioncontrol/maniserver.py
+++ src/motioncontrol/maniserver.py
@@ -1,33 +1,33 @@
 """Server side script that communicates with SES"""
 
 
+import time
+import threading
+
 import zmq
 from qtpy import QtCore
-import time
+
 # from constants import CRYO_PORT, MG15_PORT, SLIT_PORT, SLIT_TABLE
 
 
-commands = [
-    "?STATUS",
-    "?X",
-    "?Y",
-    "?Z",
-    "?P",
-    "?T",
-    "?A",
-    "MOVE X 0.123"
-]
+commands = ["? STATUS", "? X", "? Y", "? Z", "? P", "? T", "? A", "MOVE X 0.123"]
+
 
 class ManiServer(QtCore.QThread):
     PORT = 42623
     sigSocketBound = QtCore.Signal()
     sigSocketClosed = QtCore.Signal()
-    
+
     sigMove = QtCore.Signal(object, float)
 
     def __init__(self):
         super().__init__()
         self.running: bool = False
+        
+        self._ret_val = None
+        
+    def set_value(self, value):
+        self._ret_val = value
 
     def run(self):
         self.running = True
@@ -37,16 +37,20 @@ class ManiServer(QtCore.QThread):
         socket: zmq.Socket = context.socket(zmq.REP)
         socket.bind(f"tcp://*:{self.PORT}")
         self.sigSocketBound.emit()
-
         while self.running:
             try:
-                message = socket.recv(flags=zmq.NOBLOCK)
+                message: list[str] = (
+                    socket.recv(flags=zmq.NOBLOCK).decode("utf-8").split()
+                )
             except zmq.error.Again:
                 time.sleep(0.1)
                 continue
             else:
-                pass
-
+                if message[0] == "?":
+                    print(message[1])
+                elif message[0] == "MOVE":
+                    print(message[1], message[2])
+                else:
+                    socket.send(b"")
         socket.close()
         self.sigSocketClosed.emit()
-
