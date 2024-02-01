a7c47c0469139111d0798b6d7af8d7ca136d3b23
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-02-01 22:55:09 +0900

    feat: add MG15 monitor

---
+++ src/mg15/main.py
@@ -0,0 +1,321 @@
+import configparser
+import csv
+import datetime
+import json
+import multiprocessing
+import os
+import sys
+import time
+from PySide6.QtGui import QCloseEvent
+
+import numpy as np
+import pymodbus
+import pyqtgraph as pg
+from pyqtgraph.dockarea.Dock import Dock
+from pyqtgraph.dockarea.DockArea import DockArea
+from qtpy import QtCore, QtGui, QtWidgets, uic
+
+import mg15
+
+try:
+    os.chdir(sys._MEIPASS)
+except:
+    pass
+
+
+MBAR_TO_TORR: float = 76000 / 101325
+
+
+class LoggingProc(multiprocessing.Process):
+    def __init__(self, log_dir: str | os.PathLike):
+        super().__init__()
+        self.log_dir = log_dir
+        self._stopped = multiprocessing.Event()
+        self.queue = multiprocessing.Queue()
+
+    def run(self):
+        self._stopped.clear()
+        while not self._stopped.is_set():
+            time.sleep(0.02)
+
+            if self.queue.empty():
+                continue
+
+            # retrieve message from queue
+            dt, msg = self.queue.get()
+            try:
+                with open(
+                    os.path.join(self.log_dir, dt.strftime("%y%m%d") + ".csv"),
+                    "a",
+                    newline="",
+                ) as f:
+                    writer = csv.writer(f)
+                    writer.writerow([dt.isoformat()] + msg)
+            except PermissionError:
+                # put back the retrieved message in the queue
+                n_left = int(self.queue.qsize())
+                self.queue.put((dt, msg))
+                for _ in range(n_left):
+                    self.queue.put(self.queue.get())
+                continue
+
+    def stop(self):
+        n_left = int(self.queue.qsize())
+        if n_left != 0:
+            print(
+                f"Failed to write {n_left} data "
+                + ("entries:" if n_left > 1 else "entry:")
+            )
+            for _ in range(n_left):
+                dt, msg = self.queue.get()
+                print(f"{dt} | {msg}")
+        self._stopped.set()
+        self.join()
+
+    def append(self, timestamp: datetime.datetime, content):
+        if isinstance(content, str):
+            content = [content]
+        self.queue.put((timestamp, content))
+
+
+class PressuresWidget(*uic.loadUiType("pressures.ui")):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+    def set_label(self, index: int, label: str):
+        self.groups[index].setTitle(label)
+
+    def set_value(self, index: int, value: str):
+        self.labels[index].setText(value)
+
+    @property
+    def groups(self) -> tuple[QtWidgets.QGroupBox, ...]:
+        return self.group1, self.group2, self.group3
+
+    @property
+    def labels(self) -> tuple[QtWidgets.QLabel, ...]:
+        return self.label1, self.label2, self.label3
+
+
+class PlottingWidget(*uic.loadUiType("plotting.ui")):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.plots = (
+            pg.PlotDataItem(pen="c"),
+            pg.PlotDataItem(pen="m"),
+            pg.PlotDataItem(pen="y"),
+        )
+        for plot in self.plots:
+            self.plotItem.addItem(plot)
+        self.plotItem.setAxisItems({"bottom": pg.DateAxisItem()})
+
+    def set_data(
+        self,
+        x: list[datetime.datetime],
+        ylist: list[list[float]],
+    ):
+        if isinstance(self.plotItem.getAxis("bottom"), pg.DateAxisItem):
+            if self.relative_check.isChecked():
+                self.plotItem.setAxisItems({"bottom": pg.AxisItem()})
+        elif not self.relative_check.isChecked():
+            self.plotItem.setAxisItems({"bottom": pg.DateAxisItem()})
+
+        if self.relative_check.isChecked():
+            t0 = x[0]
+            xval: list[float] = [(t - t0).total_seconds() for t in x]
+        else:
+            xval: list[float] = [t.timestamp() for t in x]
+
+        for plot, yval in zip(self.plots, ylist):
+            plot.setData(xval, yval)
+
+    @property
+    def plotItem(self) -> pg.PlotItem:
+        return self.plotwidget.plotItem
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Read config file
+        self.config = configparser.ConfigParser()
+        self.config.read(QtCore.QSettings("erlab", "mg15").value("config_file"))
+
+        # Get parameters from config file
+        self.main_gauges: list[int] = json.loads(
+            self.config.get("Python", "Main Gauges", fallback="[1, 2, 4]")
+        )[:3]
+        self.channel_names: list[str] = [
+            self.config.get("Section 2", f"MG15 CH{i+1} Name")[1:-1] for i in range(7)
+        ]
+        log_dir: str = self.config.get(
+            "Python", "Logging Directory", fallback="D:\\MG15_Log"
+        )
+        log_interval: float = self.config.getfloat(
+            "Python", "Logging Interval (s)", fallback=10.0
+        )
+        address: str = self.config["Section 1"]["MG15 Path"][1:-1]
+
+        # Setup frontend docks
+        area = DockArea()
+        self.setCentralWidget(area)
+        d1 = Dock("Pressures", size=(500, 400))
+        d2 = Dock("Plot", size=(500, 400))
+        area.addDock(d2, "left")
+        area.addDock(d1, "above", d2)
+
+        # Setup frontend widgets
+        self.pressure_widget = PressuresWidget()
+        for i, idx in enumerate(self.main_gauges):
+            self.pressure_widget.set_label(i, self.channel_names[idx - 1])
+        d1.addWidget(self.pressure_widget)
+        self.plotting = PlottingWidget()
+        self.plotting.interval_spin.setValue(log_interval)
+        self.plotting.interval_spin.valueChanged.connect(self.set_logging_interval)
+        d2.addWidget(self.plotting)
+
+        # Setup data array
+        self.time_list: list[datetime.datetime] = []
+        self.pressure_list: list[list[float]] = []
+
+        # Connect to MG15
+        self.mg15 = mg15.MG15(address)
+        self.mg15.sigUpdated.connect(self.update_values)
+
+        # Setup log writing process
+        self.log_writer = LoggingProc(log_dir)
+        self.log_writer.start()
+
+        # Setup logging timer
+        self.log_timer = QtCore.QTimer(self)
+        self.set_logging_interval(log_interval, update_config=False)
+        self.log_timer.timeout.connect(self.write_log)
+
+    @QtCore.Slot(bool)
+    def toggle_updates(self, value: bool):
+        if value:
+            self.log_timer.start()
+        else:
+            self.log_timer.stop()
+
+    @QtCore.Slot()
+    def write_log(self):
+        updated: datetime.datetime = self.mg15.updated
+        pressures: list[float] = [p * MBAR_TO_TORR for p in self.mg15.pressures]
+        self.log_writer.append(updated, pressures)
+
+        # Setup plotting
+        self.time_list.append(updated)
+        main_gauge_pressures = []
+        for ch in self.main_gauges:
+            state = self.mg15.get_state(ch)
+            if state == mg15.GAUGE_STATE[0]:
+                main_gauge_pressures.append(pressures[ch - 1])
+            else:
+                main_gauge_pressures.append(np.nan)
+        self.pressure_list.append(main_gauge_pressures)
+        self.plotting.set_data(
+            self.time_list, [list(i) for i in zip(*self.pressure_list)]
+        )
+
+    @QtCore.Slot(float)
+    def set_logging_interval(self, value: float, update_config: bool = True):
+        self.log_timer.setInterval(round(value * 1000))
+        if update_config:
+            self.config.set("Python", "Logging Interval (s)", str(value))
+            with open(QtCore.QSettings("erlab", "mg15").value("config_file"), "w") as f:
+                self.config.write(f)
+
+    @QtCore.Slot()
+    def update_values(self):
+        for i, ch in enumerate(self.main_gauges):
+            status: str = self.mg15.get_state(ch)
+            if status == mg15.GAUGE_STATE[0]:
+                value = (
+                    np.format_float_scientific(
+                        self.mg15.get_pressure(ch) * MBAR_TO_TORR, 3
+                    )
+                    .replace("e", "E")
+                    .replace("-", "−")
+                )
+            else:
+                value = status
+            self.pressure_widget.set_value(i, value)
+
+    def closeEvent(self, *args, **kwargs):
+        self.log_writer.stop()
+        self.mg15.disconnect()
+        return super().closeEvent(*args, **kwargs)
+
+
+class ConfigFileDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Choose config.ini file")
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.buttonBox = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        )
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+        box = QtWidgets.QWidget()
+        box.setLayout(QtWidgets.QHBoxLayout())
+        box.layout().setContentsMargins(0, 0, 0, 0)
+        self.line = QtWidgets.QLineEdit()
+        button = QtWidgets.QPushButton("Choose File")
+        button.clicked.connect(self.choose_file)
+        box.layout().addWidget(self.line)
+        box.layout().addWidget(button)
+        self.layout().addWidget(box)
+        self.layout().addWidget(self.buttonBox)
+
+    def accept(self):
+        if valid_config(self.line.text()):
+            QtCore.QSettings("erlab", "mg15").setValue("config_file", self.line.text())
+        else:
+            QtWidgets.QMessageBox.critical(
+                self,
+                "Invalid Config File",
+                f"An error occurred while parsing the file.",
+            )
+        super().accept()
+
+    def choose_file(self):
+        dialog = QtWidgets.QFileDialog(self)
+        dialog.setAcceptMode(QtWidgets.QFileDialog.AcceptMode.AcceptOpen)
+        dialog.setFileMode(QtWidgets.QFileDialog.FileMode.ExistingFile)
+        dialog.setNameFilter("Configuration file (*.ini)")
+
+        if dialog.exec():
+            self.line.setText(dialog.selectedFiles()[0])
+
+
+def valid_config(filename: str | None = None) -> bool:
+    """Return True if a valid config file has been added, False otherwise."""
+    if filename is None:
+        filename = QtCore.QSettings("erlab", "mg15").value("config_file", "")
+    config = configparser.ConfigParser()
+    try:
+        return len(config.read(filename)) != 0
+    except configparser.ParsingError:
+        return False
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    qapp = QtWidgets.QApplication(sys.argv)
+    # qapp.setWindowIcon(QtGui.QIcon("./icon.ico"))
+
+    while not valid_config():
+        configdialog = ConfigFileDialog()
+        if configdialog.exec() != QtWidgets.QDialog.Accepted:
+            break
+    else:
+        win = MainWindow()
+        win.show()
+        win.activateWindow()
+        qapp.exec()
---
+++ src/mg15/mg15.py
@@ -0,0 +1,203 @@
+import datetime
+import struct
+import threading
+import time
+
+from pymodbus.client import ModbusTcpClient
+from qtpy import QtCore
+
+GAUGE_STATE: dict[int, str] = {
+    0: "Status OK",
+    1: "Wait for emission",
+    2: "Emission OFF",
+    3: "Short cathode",
+    4: "Pressure too high (out of vacuum gauge range)",
+    5: "Anode voltage failure",
+    6: "Bias voltage failure",
+    7: "Reflector voltage failure",
+    8: "Degas failure",
+    9: "Gauge not calibrated",
+    10: "No emission",
+    11: "Offset calibration",
+    12: "Low vacuum (out of vacuum gauge range)",
+    13: "High vacuum (out of vacuum gauge range)",
+    14: "There is no meaningful vacuum value to display",
+    15: "No hardware for passive vacuum gauge",
+    16: "Passive gauge EEPROM failure",
+    17: "Filament failure",
+    18: "Degassing",
+    19: "Sensor break for active vacuum gauge",
+}
+SETPOINT_SOURCE: dict[int, str] = {
+    1: "Vacuum channel 1",
+    2: "Vacuum channel 2",
+    3: "Vacuum channel 3",
+    4: "Vacuum channel 4",
+    5: "Vacuum channel 5",
+    6: "Vacuum channel 6",
+    7: "Vacuum channel 7",
+    100: "Always ON",
+    101: "Always OFF",
+}
+ACTIVE_GAUGE_TYPE: dict[int, str] = {
+    0: "CTR90",
+    1: "TTR90",
+    2: "TTR211",
+    3: "PTR225",
+    4: "ITR90",
+    5: "ITR100",
+    6: "MKS870",
+    7: "PTR90",
+    8: "ANALOG_IN",
+    9: "MKS_937A",
+    10: "PG105",
+    11: "MG13_14",
+    12: "PKR251/360/361",
+    13: "PCR280/TPR28x",
+    14: "ATMION",
+    15: "User Defined Vacuum Gauge",
+    16: "IKR360/361",
+}
+PASSIVE_GAUGE_TYPE: dict[int, str] = {
+    0: "UHV8A",
+    1: "IE514",
+    2: "IE414",
+    3: "UHV24",
+    4: "UHV24p",
+    5: "MKS274",
+    6: "NR_F_UHV",
+    7: "G8130",
+    8: "User Defined Passive Vacuum Gauge",
+    9: "BARION basic II",
+}
+
+
+def uint16_to_boolean_array(uint16_value):
+    # Convert uint16 to binary, remove the '0b' prefix, and pad with zeros
+    return [bit == "1" for bit in bin(uint16_value)[2:].zfill(16)]
+
+
+def uint8_to_ieee754(array):
+    # Convert two uint16 into single uint32, then unpack to float (IEEE-754)
+    return struct.unpack(">f", struct.pack(">I", (array[0] << 16) | array[1]))[0]
+
+
+class MG15Connection(QtCore.QThread):
+    sigRead = QtCore.Signal(object, object, bool)
+
+    def __init__(self, address: str):
+        super().__init__()
+        self.set_address(address)
+        self.stopped = threading.Event()
+
+    @QtCore.Slot(str)
+    def set_address(self, address: str):
+        if self.isRunning():
+            self.mutex.lock()
+        self.address = address
+        if self.isRunning():
+            self.mutex.unlick()
+
+    # @QtCore.Slot(object)
+    # def set_value(self, value):
+    #     self.mutex.lock()
+    #     self._ret_val = value
+    #     self.mutex.unlock()
+
+    def run(self):
+        self.mutex = QtCore.QMutex()
+        self.set_value(None)
+        self.stopped.clear()
+
+        client = ModbusTcpClient(self.address, 502)  # Create client object
+        client.connect()  # connect to device, reconnect automatically
+
+        while not self.stopped.is_set():
+            registers: list[int] = client.read_holding_registers(0, 125).registers
+            registers += client.read_holding_registers(125, 76).registers
+            remote_enabled: bool = bool(
+                client.read_holding_registers(1100, 1).registers[0]
+            )
+
+            self.sigRead.emit(datetime.datetime.now(), registers, remote_enabled)
+            time.sleep(0.175)
+
+        client.close()  # Disconnect device
+
+
+class MG15(QtCore.QObject):
+    sigUpdated = QtCore.Signal()
+
+    def __init__(self, address: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.connection = MG15Connection(address)
+        self.connection.sigRead.connect(self.update)
+        # self.connect()
+
+    @QtCore.Slot(object, object, bool)
+    def update(self, updatetime: datetime.datetime, registers: list[int], remote: bool):
+        self.updated: datetime.datetime = updatetime
+        self.registers: list[int] = registers
+        self.remote_enabled: bool = remote
+        self.sigUpdated.emit()
+
+    @property
+    def pressures(self) -> list[float]:
+        return [self.get_pressure(ch) for ch in range(1, 7 + 1)]
+
+    @property
+    def states(self) -> list[str]:
+        return [self.get_state(ch) for ch in range(1, 7 + 1)]
+
+    @property
+    def setpoints(self) -> list[tuple[float, float]]:
+        return [self.get_setpoint(num) for num in range(1, 10 + 1)]
+
+    @property
+    def setpoints_enabled(self) -> list[bool]:
+        return uint16_to_boolean_array(self.registers[21])[::-1][:10]
+
+    @property
+    def setpoints_source(self) -> list[str]:
+        return [self.get_setpoint_source(num) for num in range(1, 10 + 1)]
+
+    def get_pressure(self, channel: int) -> float:
+        idx = channel - 1
+        return uint8_to_ieee754(self.registers[3 * idx : 3 * idx + 2])
+
+    def get_state(self, channel: int) -> str:
+        idx = channel - 1
+        return GAUGE_STATE[self.registers[3 * idx + 2]]
+
+    def get_emission(self, channel: int) -> bool:
+        idx = channel - 1
+        if idx < 4:
+            return self.registers[72 + idx * 20]
+        else:
+            return self.registers[149 + (idx - 4) * 17]
+
+    def get_setpoint(self, number: int) -> tuple[float, float]:
+        idx = number - 1
+        vals = self.registers[4 * idx + 22 : 4 * idx + 26]
+        return uint8_to_ieee754(vals[:2]), uint8_to_ieee754(vals[2:])
+
+    def get_setpoint_enabled(self, number: int) -> bool:
+        return self.setpoints_enabled[number - 1]
+
+    def get_setpoint_source(self, number: int) -> str:
+        return SETPOINT_SOURCE[self.registers[62 + number - 1]]
+
+    def set_address(self, address: str):
+        self.connection.set_address(address)
+
+    def connect(self):
+        if not self.connection.isRunning():
+            self.connection.start()
+
+    def disconnect(self):
+        self.connection.stopped.set()
+        self.connection.wait()
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
