0f2952ae303d8727ab60e3734cfdcaf3ab067ca1
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-02-16 17:59:12 +0900

    feat: initial tempcontroller backend

--- src/tempcontroller/main.py
+++ src/tempcontroller/main.py
@@ -16,8 +16,7 @@ from pyqtgraph.dockarea.DockArea import DockArea
 from qtpy import QtCore, QtGui, QtWidgets, uic
 
 from connection import LakeshoreThread
-from widgets import HeaterWidget, QHLine, QVLine
-from widgets import ReadingWidgetGUI as ReadingWidget
+from widgets import HeaterWidget, QHLine, QVLine, ReadingWidget, CommandWidget
 
 try:
     os.chdir(sys._MEIPASS)
@@ -25,7 +24,62 @@ except:
     pass
 
 
-class MainWindow(*uic.loadUiType("main.ui")):
+class LoggingProc(multiprocessing.Process):
+    def __init__(self, log_dir: str | os.PathLike):
+        super().__init__()
+        self.log_dir = log_dir
+        self._stopped = multiprocessing.Event()
+        self.queue = multiprocessing.Queue()
+
+    def run(self):
+        self._stopped.clear()
+        while not self._stopped.is_set():
+            time.sleep(0.02)
+
+            if self.queue.empty():
+                continue
+
+            # retrieve message from queue
+            dt, msg, is_header = self.queue.get()
+            try:
+                with open(
+                    os.path.join(self.log_dir, dt.strftime("%y%m%d") + ".csv"),
+                    "a",
+                    newline="",
+                ) as f:
+                    writer = csv.writer(f)
+                    if is_header:
+                        writer.writerow(msg)
+                    else:
+                        writer.writerow([dt.isoformat()] + msg)
+            except PermissionError:
+                # put back the retrieved message in the queue
+                n_left = int(self.queue.qsize())
+                self.queue.put((dt, msg, is_header))
+                for _ in range(n_left):
+                    self.queue.put(self.queue.get())
+                continue
+
+    def stop(self):
+        n_left = int(self.queue.qsize())
+        if n_left != 0:
+            print(
+                f"Failed to write {n_left} data "
+                + ("entries:" if n_left > 1 else "entry:")
+            )
+            for _ in range(n_left):
+                dt, msg = self.queue.get()
+                print(f"{dt} | {msg}")
+        self._stopped.set()
+        self.join()
+
+    def append(self, timestamp: datetime.datetime, content, is_header: bool = False):
+        if isinstance(content, str):
+            content = [content]
+        self.queue.put((timestamp, content, is_header))
+
+
+class MainWindowGUI(*uic.loadUiType("main.ui")):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
@@ -40,17 +94,17 @@ class MainWindow(*uic.loadUiType("main.ui")):
         self.readings_336 = ReadingWidget(
             inputs=("A", "B", "C", "D"),
             names=self.config["config"]["names_336"],
-            hide_srdg=True,
+            decimals=3,
         )
         self.readings_218_0 = ReadingWidget(
             inputs=tuple(str(i) for i in range(1, 5)),
             names=list(self.config["config"]["names_218"])[:4],
-            hide_srdg=True,
+            indexer=slice(0, 4),
         )
         self.readings_218_1 = ReadingWidget(
             inputs=tuple(str(i) for i in range(5, 9)),
             names=list(self.config["config"]["names_218"])[4:],
-            hide_srdg=True,
+            indexer=slice(4, 8),
         )
 
         readings_218_combined = QtWidgets.QWidget()
@@ -60,7 +114,11 @@ class MainWindow(*uic.loadUiType("main.ui")):
         readings_218_combined.layout().addWidget(QVLine())
         readings_218_combined.layout().addWidget(self.readings_218_1)
         self.readings_331 = ReadingWidget(
-            inputs=(" ",), names=self.config["config"]["names_331"], hide_srdg=True
+            inputs=(" ",),
+            names=self.config["config"]["names_331"],
+            hide_srdg=True,
+            krdg_command="KRDG? B",
+            srdg_command="SRDG? B",
         )
 
         self.group0.layout().addWidget(self.readings_336)
@@ -79,19 +137,128 @@ class MainWindow(*uic.loadUiType("main.ui")):
         self.heater1 = HeaterWidget()
         self.heater2 = HeaterWidget()
         self.heater3 = HeaterWidget()
-        d4 = Dock(f"336 Heater1 (D) Control", widget=self.heater1)
-        d5 = Dock(f"336 Heater2 (C) Control", widget=self.heater2)
-        d6 = Dock(f"331 Heater Control", widget=self.heater3)
+        d1 = Dock(f"336 Heater1 (D) Control", widget=self.heater1)
+        d2 = Dock(f"336 Heater2 (C) Control", widget=self.heater2)
+        d3 = Dock(f"331 Heater Control", widget=self.heater3)
+        self.heaters.addDock(d2, "left")
+        self.heaters.addDock(d1, "right", d2)
+        self.heaters.addDock(d3, "right", d1)
+
+        self.commands = DockArea()
+        # self.commands.setWindowTitle("")
+        self.command336 = CommandWidget()
+        self.command218 = CommandWidget()
+        self.command331 = CommandWidget()
+        d4 = Dock(f"336 Command", widget=self.command336)
+        d5 = Dock(f"218 Command", widget=self.command218)
+        d6 = Dock(f"331 Command", widget=self.command331)
         self.heaters.addDock(d5, "left")
         self.heaters.addDock(d4, "right", d5)
         self.heaters.addDock(d6, "right", d4)
 
         self.actionheaters.triggered.connect(lambda: self.heaters.show())
+        self.actioncommand.triggered.connect(lambda: self.commands.show())
+        self.actionvoltage.triggered.connect(self.toggle_voltages)
 
         self.resize(100, 100)
 
+    def overwrite_config(self):
+        with open(
+            QtCore.QSettings("erlab", "tempcontroller").value("config_file"), "w"
+        ) as f:
+            tomlkit.dump(self.config, f)
+
+    def toggle_voltages(self):
+        for rw in (
+            self.readings_336,
+            self.readings_218_0,
+            self.readings_218_1,
+            self.readings_331,
+        ):
+            rw.set_srdg_visible(not rw.srdg_enabled)
+
     def closeEvent(self, *args, **kwargs):
+        self.heaters.close()
+        self.commands.close()
+        super().closeEvent(*args, **kwargs)
+
+
+class MainWindow(MainWindowGUI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Initialize temperature controller threads
+        self.lake218 = LakeshoreThread("GPIB0::12::INSTR", baud_rate=9600)
+        self.lake331 = LakeshoreThread("GPIB0::15::INSTR", baud_rate=9600)
+        self.lake336 = LakeshoreThread("GPIB0::18::INSTR", baud_rate=57600)
+
+        self.readings_331.instrument = self.lake331
+        self.readings_218_0.instrument = self.lake218
+        self.readings_218_1.instrument = self.lake218
+        self.readings_336.instrument = self.lake336
+
+        self.command336.instrument = self.lake336
+        self.command218.instrument = self.lake218
+        self.command331.instrument = self.lake331
+
+        self.lake218.start()
+        self.lake331.start()
+        self.lake336.start()
+
+        # Setup refresh timer
+        refresh_time = float(self.config["config"]["refresh_time"])
+        self.refresh_timer = QtCore.QTimer(self)
+        self.refresh_timer.setInterval(round(refresh_time * 1000))
+        self.refresh_timer.timeout.connect(self.update)
+
+        # Get logging parameters from config
+        log_dir = str(self.config["logging"]["directory"])
+        log_interval = float(self.config["logging"]["interval"])
+
+        # Setup log writing process
+        self.log_writer = LoggingProc(log_dir)
+        self.log_writer.start()
+
+        # Setup logging timer
+        self.log_timer = QtCore.QTimer(self)
+        self.set_logging_interval(log_interval, update_config=False)
+        self.log_timer.timeout.connect(self.write_log)
+        self.toggle_updates(True)
+
+        # Start acquiring & logging
+        self.refresh_timer.start()
+        self.log_timer.start()
+
+    def set_logging_interval(self, value: float, update_config: bool = True):
+        self.log_timer.setInterval(round(value * 1000))
+        if update_config:
+            self.config["logging"]["interval"] = value
+            self.overwrite_config()
+
+    def update(self):
+        # UPDATE HEATERS HERE
+        for rdng in (
+            self.readings_331,
+            self.readings_218_0,
+            self.readings_218_1,
+            self.readings_336,
+        ):
+            rdng.trigger_update()
+
+    def write_log(self):
+        dt =  datetime.datetime.now()
+        # self.log_writer.append(dt, )
+        pass
+
+    def closeEvent(self, *args, **kwargs):
+        self.lake218.stopped.set()
+        self.lake331.stopped.set()
+        self.lake336.stopped.set()
+        self.lake218.wait()
+        self.lake331.wait()
+        self.lake336.wait()
         pyvisa.ResourceManager().close()
+        self.log_writer.stop()
         super().closeEvent(*args, **kwargs)
 
 
@@ -161,7 +328,7 @@ if __name__ == "__main__":
 
     qapp = QtWidgets.QApplication(sys.argv)
     # qapp.setWindowIcon(QtGui.QIcon("./icon.ico"))
-    # qapp.setStyle("Fusion")
+    qapp.setStyle("Fusion")
 
     while not valid_config():
         configdialog = ConfigFileDialog()
--- src/tempcontroller/widgets.py
+++ src/tempcontroller/widgets.py
@@ -161,46 +161,134 @@ class SingleReadingWidget(*uic.loadUiType("reading.ui")):
         self.srdg.setText(str(value))
 
 
+# class ReadingWidgetGUI(QtWidgets.QWidget):
+#     def __init__(
+#         self,
+#         *args,
+#         inputs: Sequence[str],
+#         names: Sequence[str] | None = None,
+#         hide_srdg: bool = False,
+#         **kwargs,
+#     ):
+#         super().__init__(*args, **kwargs)
+#         self.setLayout(QtWidgets.QVBoxLayout())
+#         self.readingwidgets: list[SingleReadingWidget] = []
+#         for input in inputs:
+#             self.readingwidgets.append(
+#                 SingleReadingWidget(input=input, hide_srdg=hide_srdg)
+#             )
+#             self.layout().addWidget(self.readingwidgets[-1])
+
+#         if names is not None:
+#             self.update_names(names)
+
+
 class ReadingWidgetGUI(QtWidgets.QWidget):
     def __init__(
         self,
         *args,
         inputs: Sequence[str],
         names: Sequence[str] | None = None,
-        hide_srdg: bool = False,
+        hide_srdg: bool = True,
+        decimals: int = 2,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        self.setLayout(QtWidgets.QVBoxLayout())
-        self.readingwidgets: list[SingleReadingWidget] = []
-        for input in inputs:
-            self.readingwidgets.append(
-                SingleReadingWidget(input=input, hide_srdg=hide_srdg)
-            )
-            self.layout().addWidget(self.readingwidgets[-1])
+        self.setLayout(QtWidgets.QGridLayout())
+        self.name_labels: list[QtWidgets.QLabel] = []
+        self.krdg_spins: list[QtWidgets.QDoubleSpinBox] = []
+        self.srdg_spins: list[QtWidgets.QDoubleSpinBox] = []
+        self.krdg_units: list[QtWidgets.QLabel] = []
+        self.srdg_units: list[QtWidgets.QLabel] = []
 
+        boldfont = QtGui.QFont()
+        boldfont.setBold(True)
+
+        for i, input in enumerate(inputs):
+            input_label = QtWidgets.QLabel(input)
+            input_label.setFont(boldfont)
+
+            name_label = QtWidgets.QLabel()
+            name_label.setWordWrap(True)
+
+            krdg_spin = QtWidgets.QDoubleSpinBox()
+            krdg_spin.setReadOnly(True)
+            krdg_spin.setDecimals(decimals)
+            # krdg_spin.setSuffix(" K")
+            krdg_spin.setRange(0.0, 500.0)
+            krdg_spin.setButtonSymbols(krdg_spin.ButtonSymbols.NoButtons)
+
+            srdg_spin = QtWidgets.QDoubleSpinBox()
+            srdg_spin.setReadOnly(True)
+            srdg_spin.setDecimals(decimals)
+            # srdg_spin.setSuffix(" V")
+            srdg_spin.setRange(0.0, 500.0)
+            srdg_spin.setButtonSymbols(srdg_spin.ButtonSymbols.NoButtons)
+
+            krdg_unit = QtWidgets.QLabel("[K]")
+            srdg_unit = QtWidgets.QLabel("[V]")
+
+            self.layout().addWidget(input_label, 2 * i, 0, 2, 1)
+            self.layout().addWidget(name_label, 2 * i, 1, 2, 3)
+            self.layout().addWidget(krdg_spin, 2 * i, 4, 1, 2)
+            self.layout().addWidget(srdg_spin, 2 * i + 1, 4, 1, 2)
+            self.layout().addWidget(krdg_unit, 2 * i, 6, 1, 1)
+            self.layout().addWidget(srdg_unit, 2 * i + 1, 6, 1, 1)
+
+            self.name_labels.append(name_label)
+            self.krdg_spins.append(krdg_spin)
+            self.srdg_spins.append(srdg_spin)
+            self.krdg_units.append(krdg_unit)
+            self.srdg_units.append(srdg_unit)
+
+        self.set_srdg_visible(not hide_srdg)
         if names is not None:
             self.update_names(names)
 
     @property
     def srdg_enabled(self) -> bool:
-        return self.readingwidgets[0].srdg.isVisible()
+        # return self.readingwidgets[0].srdg.isVisible()
+        return self.srdg_spins[0].isVisible()
+
+    @property
+    def krdg(self) -> list[float]:
+        return [spin.value() for spin in self.krdg_spins]
+
+    @property
+    def srdg(self) -> list[float]:
+        return [spin.value() for spin in self.srdg_spins]
 
     def set_srdg_visible(self, visible: bool):
-        for rw in self.readingwidgets:
-            rw.set_srdg_visible(visible)
+        for i in range(len(self.krdg_spins)):
+
+            self.srdg_spins[i].setVisible(visible)
+            self.srdg_units[i].setVisible(visible)
+            if visible:
+                self.layout().addWidget(self.krdg_spins[i], 2 * i, 4, 1, 2)
+                self.layout().addWidget(self.krdg_units[i], 2 * i, 6, 1, 1)
+            else:
+                self.layout().addWidget(self.krdg_spins[i], 2 * i, 4, 2, 2)
+                self.layout().addWidget(self.krdg_units[i], 2 * i, 6, 2, 1)
+        # for rw in self.readingwidgets:
+        # rw.set_srdg_visible(visible)
 
     def update_names(self, names: list[str]):
-        for w, name in zip(self.readingwidgets, names):
-            w.set_name(name)
+        for label, name in zip(self.name_labels, names):
+            label.setText(name)
+        # for w, name in zip(self.readingwidgets, names):
+        # w.set_name(name)
 
     def update_krdg(self, readings: list[float]):
-        for w, rdg in zip(self.readingwidgets, readings):
-            w.set_krdg(rdg)
+        for spin, value in zip(self.krdg_spins, readings):
+            spin.setValue(value)
+        # for w, rdg in zip(self.readingwidgets, readings):
+        # w.set_krdg(rdg)
 
     def update_srdg(self, readings: list[float]):
-        for w, rdg in zip(self.readingwidgets, readings):
-            w.set_srdg(rdg)
+        for spin, value in zip(self.srdg_spins, readings):
+            spin.setValue(value)
+        # for w, rdg in zip(self.readingwidgets, readings):
+        #     w.set_srdg(rdg)
 
 
 class ReadingWidget(ReadingWidgetGUI):
@@ -209,26 +297,44 @@ class ReadingWidget(ReadingWidgetGUI):
     sigSRDG = QtCore.Signal(str)
 
     def __init__(
-        self, *args, instrument: LakeshoreThread, inputs: Sequence[str], **kwargs
+        self,
+        *args,
+        instrument: LakeshoreThread | None = None,
+        inputs: Sequence[str],
+        indexer: slice | None = None,
+        krdg_command: str | None = None,
+        srdg_command: str | None = None,
+        **kwargs,
     ):
         super().__init__(*args, inputs=inputs, **kwargs)
         self.instrument = instrument
-
+        self.indexer = indexer
+        if krdg_command is None:
+            krdg_command = "KRDG? 0"
+        if srdg_command is None:
+            srdg_command = "SRDG? 0"
+        self.krdg_command = krdg_command
+        self.srdg_command = srdg_command
         self.sigKRDG.connect(self.update_krdg)
         self.sigSRDG.connect(self.update_srdg)
 
     def trigger_update(self):
-        self.instrument.request_query("KRDG? 0", self.sigKRDG)
-        if self.srdg_enabled:
-            self.instrument.request_query("SRDG? 0", self.sigSRDG)
+        self.instrument.request_query(self.krdg_command, self.sigKRDG)
+        self.instrument.request_query(self.srdg_command, self.sigSRDG)
 
     @QtCore.Slot(str)
     def update_krdg(self, message):
-        super().update_krdg([float(t) for t in message.split(",")])
+        vals = [float(t) for t in message.split(",")]
+        if self.indexer is not None:
+            vals = vals[self.indexer]
+        super().update_krdg(vals)
 
     @QtCore.Slot(str)
     def update_srdg(self, message):
-        super().update_srdg([float(t) for t in message.split(",")])
+        vals = [float(t) for t in message.split(",")]
+        if self.indexer is not None:
+            vals = vals[self.indexer]
+        super().update_srdg(vals)
 
 
 class CommandWidget(*uic.loadUiType("command.ui")):
@@ -236,7 +342,7 @@ class CommandWidget(*uic.loadUiType("command.ui")):
     sigQuery = QtCore.Signal(str)
     sigReply = QtCore.Signal(str)
 
-    def __init__(self, instrument: LakeshoreThread=None, parent=None):
+    def __init__(self, instrument: LakeshoreThread | None = None, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.instrument = instrument
